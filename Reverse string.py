d4b5e008393306b4f5deaea4dec9eb7f36a5dd6b
Vanshraj Surendra Chauhan <8460630314m@gmail.com>
2024-12-03 18:08:14 +0530

    Add files via upload

---
+++ Reverse string.py
@@ -0,0 +1,11 @@
+a="Geeks"
+b=list(a)       #string converted into list to use return function
+b.reverse()     #return fuction()
+print("".join (b))  #Join(b) joins list into a string and ("") means we does not want anything instead of commas
+
+#join function can be used if all elements are in string format
+
+                #OR
+c=a[::-1]
+print(c)             #slicing method
+                    
